2ef40fb12d44800126a3e805652e1169d4f2e38d
Thiago Aragão <tbaragao@gmail.com>
2025-06-17 12:23:14 -0300

    feat: implements button component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Icon from "./components/icon";
 import Text from "./components/text";
 import TrashIcon from "./assets/icons/trash-regular.svg?react";
 import Badge from "./components/badge";
+import Button from "./components/button";
 
 export default function App() {
 
@@ -11,6 +12,7 @@ export default function App() {
       <Icon svg={TrashIcon} className="fill-green-base"/>
       <Badge variant="primary">5</Badge>
       <Badge variant="secondary">2 de 5</Badge>
+      <Button icon={TrashIcon} type="button" >Delete</Button>
     </>
   )
 }
