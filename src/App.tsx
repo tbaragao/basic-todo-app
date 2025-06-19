8f2b42f31e9f039730c5c03b120d5320dc569a3a
Thiago Aragão <tbaragao@gmail.com>
2025-06-19 00:12:16 -0300

    feat: implements input-text component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,32 @@ import TrashIcon from "./assets/icons/trash-regular.svg?react";
 import Badge from "./components/badge";
 import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
+import InputText from "./components/input-text";
 
 export default function App() {
-
   return (
     <>
-      <Text variant="body-md-bold" className="text-green-base">Clean Project with tailwind</Text>
-      <Icon svg={TrashIcon} className="fill-green-base"/>
+      <Text variant="body-md-bold" className="text-green-base">
+        Clean Project with tailwind
+      </Text>
+      <Icon svg={TrashIcon} className="fill-green-base" />
       <Badge variant="primary">5</Badge>
       <Badge variant="secondary">2 de 5</Badge>
-      <Button icon={TrashIcon} type="button" >Delete</Button>
+      <Button icon={TrashIcon} type="button">
+        Delete
+      </Button>
       <ButtonIcon variant="primary" icon={TrashIcon} type="button"></ButtonIcon>
-      <ButtonIcon variant="secondary" icon={TrashIcon} type="button"></ButtonIcon>
-      <ButtonIcon variant="terciary" icon={TrashIcon} type="button"></ButtonIcon>
+      <ButtonIcon
+        variant="secondary"
+        icon={TrashIcon}
+        type="button"
+      ></ButtonIcon>
+      <ButtonIcon
+        variant="terciary"
+        icon={TrashIcon}
+        type="button"
+      ></ButtonIcon>
+      <InputText />
     </>
-  )
+  );
 }
-
---
+++ src/components/input-text.tsx
@@ -0,0 +1,45 @@
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import { textVariants } from "./text";
+
+export const inputTextVariants = cva(
+  "border-b border-solid border-gray-200 focus:border-pink-base bg-transparent outline-none",
+  {
+    variants: {
+      size: {
+        md: "px-2 pb-2",
+      },
+      disabled: {
+        true: "pointer-events-none",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+      disabled: false,
+    },
+  }
+);
+
+interface inputTextProps
+  extends VariantProps<typeof inputTextVariants>,
+    Omit<React.ComponentProps<"input">, "size" | "disabled"> {
+  className?: string;
+}
+
+export default function InputText({
+  size,
+  disabled,
+  className,
+  ...props
+}: inputTextProps) {
+  return (
+    <input
+      className={cx(
+        inputTextVariants({ size, disabled }),
+        textVariants(),
+        className
+      )}
+      type="text"
+      {...props}
+    />
+  );
+}
