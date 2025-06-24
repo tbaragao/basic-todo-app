7d18808640c953fb6076e0f09906611f555cac5e
Thiago Aragão <tbaragao@gmail.com>
2025-06-23 23:31:48 -0300

    feat: implements task summary component

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,5 +1,12 @@
 import Container from "../components/container";
+import TasksSummary from "../core-components/tasks-summary";
 
 export default function Home() {
-  return <Container>Home Page</Container>;
+  return (
+    <Container as="article" className="space-y-3">
+      <header className="flex items-center justify-between">
+        <TasksSummary />
+      </header>
+    </Container>
+  );
 }
